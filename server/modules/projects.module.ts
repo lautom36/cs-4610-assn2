048b329d3afb67ff7659511ebc65cca7eb4cb0e7
lautom36 <lautom36@gmail.com>
2022-02-19 17:13:24 -0700

    added task dependancy

--- server/modules/projects.module.ts
+++ server/modules/projects.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsController } from 'server/controllers/api/projects.controller';
 import { Projects } from 'server/entities/projects.entity';
+import { Tasks } from 'server/entities/tasks.entity';
 import { ProjectsService } from 'server/providers/services/projects.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Projects])],
+  imports: [TypeOrmModule.forFeature([Projects, Tasks])],
   controllers: [ProjectsController],
   providers: [ProjectsService],
   exports: [],

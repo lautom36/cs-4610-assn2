59ade004e3cd00d4b85c3141ebf5aa9b8528691b
lautom36 <lautom36@gmail.com>
2022-02-19 17:13:32 -0700

    added task dependancy

--- server/modules/projects.module.ts
+++ server/modules/projects.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsController } from 'server/controllers/api/projects.controller';
 import { Projects } from 'server/entities/projects.entity';
 import { Tasks } from 'server/entities/tasks.entity';
+import { User } from 'server/entities/user.entity';
 import { ProjectsService } from 'server/providers/services/projects.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Projects, Tasks])],
+  imports: [TypeOrmModule.forFeature([Projects, Tasks, User])],
   controllers: [ProjectsController],
   providers: [ProjectsService],
   exports: [],
--- server/modules/tasks.module.ts
+++ server/modules/tasks.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from 'server/controllers/api/tasks.controller';
+import { Projects } from 'server/entities/projects.entity';
 import { Tasks } from 'server/entities/tasks.entity';
+import { User } from 'server/entities/user.entity';
 import { TasksService } from 'server/providers/services/tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tasks])],
+  imports: [TypeOrmModule.forFeature([Tasks, Projects, User])],
   controllers: [TasksController],
   providers: [TasksService],
   exports: [],

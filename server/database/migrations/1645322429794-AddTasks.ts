622e2867015017c9b8698a5864a96039bd8e463d
Lane <lanetravisbarnes@gmail.com>
2022-02-19 19:18:10 -0700

    I added in the migration files

---
+++ server/database/migrations/1645322429794-AddTasks.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner, Table, TableColumn, TableForeignKey } from 'typeorm';
+
+export class AddTasks1645322429794 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'task',
+        columns: [
+          {
+            name: 'id',
+            isPrimary: true,
+            isGenerated: true,
+            type: 'int',
+          },
+          {
+            name: 'projectId',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'title',
+            type: 'text',
+            isNullable: false,
+          },
+          {
+            name: 'description',
+            type: 'text',
+            isNullable: false,
+          },
+          {
+            name: 'timeEstimation',
+            type: 'text',
+            isNullable: false,
+          },
+          {
+            name: 'status',
+            type: 'boolean',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'project',
+      new TableForeignKey({
+        columnNames: ['projectId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'project',
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('task');
+  }
+}

398f12927107f01a7f29118086e669c1343675e2
lautom36 <lautom36@gmail.com>
2022-02-19 17:25:20 -0700

    plz

--- server/modules/projects.module.ts
+++ server/modules/projects.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsController } from 'server/controllers/api/projects.controller';
+import { TasksController } from 'server/controllers/api/tasks.controller';
+import { UsersController } from 'server/controllers/users.controller';
 import { Projects } from 'server/entities/projects.entity';
 import { Tasks } from 'server/entities/tasks.entity';
 import { User } from 'server/entities/user.entity';
 import { ProjectsService } from 'server/providers/services/projects.service';
+import { TasksService } from 'server/providers/services/tasks.service';
+import { UsersService } from 'server/providers/services/users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Projects]), Tasks, User],
-  controllers: [ProjectsController],
-  providers: [ProjectsService],
-  exports: [],
+  imports: [TypeOrmModule.forFeature([Projects, Tasks, User])],
+  controllers: [ProjectsController, TasksController, UsersController],
+  providers: [ProjectsService, TasksService, UsersService],
+  exports: [TypeOrmModule],
 })
 export class ProjectsModule {}

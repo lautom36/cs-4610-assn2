7826e005cdbcef00d93c583d46c0d1d9a5ade25f
lautom36 <lautom36@gmail.com>
2022-02-20 14:37:47 -0700

    database runs

--- server/modules/projects.module.ts
+++ server/modules/projects.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsController } from 'server/controllers/api/projects.controller';
 import { Projects } from 'server/entities/projects.entity';
+import { Role } from 'server/entities/role.entity';
+import { User } from 'server/entities/user.entity';
+import { UserRole } from 'server/entities/user_role.entity';
 import { ProjectsService } from 'server/providers/services/projects.service';
+import { UsersService } from 'server/providers/services/users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Projects])],
+  imports: [TypeOrmModule.forFeature([Projects, User, UserRole, Role])],
   controllers: [ProjectsController],
-  providers: [ProjectsService],
+  providers: [ProjectsService, UsersService],
   exports: [],
 })
 export class ProjectsModule {}
--- server/modules/tasks.module.ts
+++ server/modules/tasks.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksController } from 'server/controllers/api/tasks.controller';
+import { Projects } from 'server/entities/projects.entity';
+import { Role } from 'server/entities/role.entity';
 import { Tasks } from 'server/entities/tasks.entity';
+import { User } from 'server/entities/user.entity';
+import { UserRole } from 'server/entities/user_role.entity';
+import { ProjectsService } from 'server/providers/services/projects.service';
 import { TasksService } from 'server/providers/services/tasks.service';
+import { UsersService } from 'server/providers/services/users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tasks])],
+  imports: [TypeOrmModule.forFeature([Tasks, Projects, User, UserRole, Role])],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService, ProjectsService, UsersService],
   exports: [],
 })
 export class TasksModule {}
